f87c374da3e49415610c7a3434a1797df64c1f05
Mons Pavel <monspavel@gmail.com>
2022-08-06 15:31:26 +0300

    improve setting function name

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,1 +1,13 @@
 export const fromCapitalLetter = (word) => word.slice(0, 1).toUpperCase() + word.slice(1, word.length)
+
+export const getPrefix = (method) => {
+    const prefixes = {
+        get: 'get',
+        post: 'add',
+        patch: 'update',
+        delete: 'delete',
+        default: ''
+    }
+
+    return prefixes[method] || prefixes.default
+}
