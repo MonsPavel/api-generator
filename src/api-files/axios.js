a604ad5b5912e5aafb0167f1a2f478b699e5af4e
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2022-08-06 19:56:31 +0300

    add base api class and axios config (#7)

---
+++ src/api-files/axios.js
@@ -0,0 +1,12 @@
+export const axios = `
+import axios from 'axios'
+
+// TODO: baseUrl Functional
+const baseURL = '/'
+
+const axiosInstance = axios.create({
+  baseURL,
+})
+
+export default axiosInstance;
+`
